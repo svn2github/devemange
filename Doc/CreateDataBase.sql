2c842f545feb67b3128092e43c3d1da5fb42b771
mrlong.com <mrlong.com@9ff6b1e3-de3f-0410-996c-bd7989423481>
2007-11-27 03:45:53 +0000

    文档说明
    git-svn-id: https://devemanage.googlecode.com/svn/trunk@13 9ff6b1e3-de3f-0410-996c-bd7989423481

---
+++ Doc/CreateDataBase.sql
@@ -0,0 +1,18 @@
+/*******************************************************
+ *
+ *  BFSS 
+ *
+ * 作者: 龙仕云  日期:2007-10-26 
+ *
+ *
+ *
+ ******************************************************/
+
+/*文件树*/
+create table TB_FILE_TREE(
+	ZID	int not null,
+	ZNAME	VarChar(20) not null,
+	ZMONTHVOLUME int default 0,             /* 月最大购买量（充值上限） */
+constraint PK_TB_MEDIA  primary key (ZID)
+)
+go
